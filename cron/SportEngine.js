b2c7dee0e265827a314991a6c38a38019b3f956e
Kevin Hagens <khagens92@gmail.com>
2019-04-02 12:00:04 -0500

    api integration

--- cron/SportEngine.js
+++ cron/SportEngine.js
@@ -12,13 +12,35 @@ async function run(){
   	database.db = db;
   	console.log('Successfully connected to mongo db, running sports engine');
 
+    const date = SportHelper.getDate();
     const events = await SportHelper.getTodaysEvents();
-    console.log('events: ' + events);
     if(!events){
+      console.log('failed to get events from API');
       return;
     }
-
+    console.log('EVENTS FOR DAY: ' + date);
     console.log(JSON.stringify(events));
+    try{
+        const dayCollection = db.collection('day');
+        const dayObject = {
+          date : date,
+          sports : events.sports
+        };
+        const result = await dayCollection
+        .find({date : date })
+  			.toArray();
+  			if(!result || result.length == 0){
+  				  await dayCollection.insertOne(dayObject);
+  			}else{
+            await dayCollection
+            .updateOne({ _id : new ObjectID(result[0]['_id'])},{ $set : dayObject }, {upsert: true});
+        }
+
+    }catch(e){
+      console.log('failed to save today event into array: ' + JSON.stringify(events));
+    }
+
+
 
   });
 
